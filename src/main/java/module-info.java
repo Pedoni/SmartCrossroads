fefb8ca92d50e886469db95dacad56c956f2e17d
Emanuele Lamagna <emanuele.lamagna@studio.unibo.it>
2025-02-06 21:15:07 +0100

    Eliminated fxml files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module it.unibo.smartcrossroads {
     requires javafx.controls;
-    requires javafx.fxml;
-
     requires org.controlsfx.controls;
 
-    opens it.unibo.smartcrossroads to javafx.fxml;
     exports it.unibo.smartcrossroads;
 }
