706244e7a051ee683e39d2a93fa6e08d221cea2b
Emanuele Lamagna <emanuele.lamagna@studio.unibo.it>
2025-03-16 15:38:21 +0100

    Removed unused constants

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -4,9 +4,7 @@ public final class Constants {
     public static final int ROAD_WIDTH = 100;
     public static final int DASH_LENGTH = 10;
     public static final int DASH_GAP = 10;
-    public static final double CAR_SCALE_FACTOR = 0.05;
     public static final double MAX_SPEED = 2;
-    public static final double TL_SCALE_FACTOR = 0.15;
     public static final int ROWS = 13;
     public static final int COLUMNS = 17;
 }
